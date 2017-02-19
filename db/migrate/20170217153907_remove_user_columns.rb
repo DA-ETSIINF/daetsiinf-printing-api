fbab96de8b583020d5f5bb8357b3fd2abf9dac8f
Alejandro Serradilla <alexserradillarufino@gmail.com>
2017-02-19 13:09:05 +0100

    Fix migration

--- db/migrate/20170217153907_remove_user_columns.rb
+++ db/migrate/20170217153907_remove_user_columns.rb
@@ -2,7 +2,6 @@ class RemoveUserColumns < ActiveRecord::Migration[5.0]
   def change
     remove_column :users, :reset_password_token
     remove_column :users, :reset_password_sent_at
-    remove_column :users, :remember_created_at
     remove_column :users, :sign_in_count
     remove_column :users, :current_sign_in_at
     remove_column :users, :last_sign_in_at
